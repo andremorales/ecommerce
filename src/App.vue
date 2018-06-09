08cd928b82ad507aa45b48fa7a06b4bbd68cdf68
André Morales <andremorales10@hotmail.com>
2018-06-09 03:31:27 -0300

    modific App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="phone-viewreport">
+  <header class="phone-viewreport">
     <md-toolbar>
-    <md-button class="md-icon button" v-on:click="">
+    <md-button class="md-icon button" v-on:click.native="toggleLeftSidenav">
       <md-icon>menu</md-icon>
     </md-button>  
 
       <h1 class="mb-title right">{{title}}</h1>
     </md-toolbar>
-  </div>
+
+    <mb-sidenav class="md-left" ref="leftSidenav">
+      <md-toolbar class="md-medium">
+        <div class="md-toolbar-container">
+          <h2 class="md-title">Sidenav content</h2>
+        </div>
+      </md-toolbar>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam laboriosam autem aliquam quis id, quasi tenetur laborum provident aliquid quisquam placeat, voluptas itaque iste dignissimos debitis reiciendis maxime sapiente beatae.
+        </p>
+    </mb-sidenav>
+  </header>
 </template>
 
 <script>
@@ -17,7 +28,8 @@ export default {
     return {
       title: "Ecommerce"
     };
-  }
+  },
+  methods: {}
 };
 </script>
 
